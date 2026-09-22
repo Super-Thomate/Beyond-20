97b8b11ec1cd7c93a0917f4e2dcc17dbc4bd96bf
Thomas Plaindoux <tomplaindoux1@orange.fr>
2026-09-22 15:14:28 +0200

    increase max level

---
+++ scripts/beyond-20.mjs
@@ -0,0 +1,7 @@
+// Script to manage advancement beyond level 20 in DnD 5E.
+// Based on Past Level 20 (5e Variant Rule) https://www.dandwiki.com/wiki/Past_Level_20_(5e_Variant_Rule)
+
+// Increase max level for Player Actor
+Hooks.once("init", () => {
+    CONFIG.DND5E.maxLevel = 50;
+})
